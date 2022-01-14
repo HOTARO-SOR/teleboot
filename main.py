e7ddc58d982b6a1313bd2335111a537c9f9b9613
سورس هوتارو <97184529+HOTARO-SOR@users.noreply.github.com>
2022-01-14 13:23:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 print('Hello world ')
-print('welcome to my source ')
 
-
+print('Welcome to my source ')
 
 from telethon.sync import TelegramClient
+
 from telethon.sessions import StringSession
+
 print("******************************************")
-APP_ID = int(input("ENTER APP ID > "))
-API_HASH = input("- ENTER API HASH > ")
+
+APP_ID = int(input("ENTER 𝙰𝙿𝙿 𝙸𝙳 ➺ > "))
+
+API_HASH = input("- ENTER 𝙰𝙿𝙸 𝙷𝙰𝚂𝙷 ➺ > ")
+
 print("\n--------------------------")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as tb:
+
     print("")
-    print("\n---------------------------")
-    print("")
-    print("You Will get STRING SESSION  in save msg")
-    jmth = tb.send_message("me", f"`{tb.session.save()}`")
-    jmth.reply('This is your cod termux by @EnYusuf')
+
+    ‏    print("\n---------------------------")
+
+    ‏    print("")
+
+    ‏    print("You Will get STRING SESSION  in save msg")
+
+    ‏    jmth = tb.send_message("me", f"`{tb.session.save()}`")
+
+    ‏    jmth.reply( 'هذا هوه كود تيرمكس @AbdSha1 ' )
+
+
+
+ 
+
+
+
 
 
